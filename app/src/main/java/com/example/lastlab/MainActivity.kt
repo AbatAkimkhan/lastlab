03756d80a112528f9c34cbf4484f8fe2c13c87d7
Akimkhan Abat <Akim.khan0524@gmail.com>
2025-04-10 00:59:19 +0500

    Add buttons to start/stop Foreground Service and navigate to second activity

--- app/src/main/java/com/example/lastlab/MainActivity.kt
+++ app/src/main/java/com/example/lastlab/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.lastlab
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import com.example.lastlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var serviceIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        serviceIntent = Intent(this, MyService::class.java)
+
+        binding.btnStartService.setOnClickListener {
+            ContextCompat.startForegroundService(this, serviceIntent)
+        }
+
+        binding.btnStopService.setOnClickListener {
+            stopService(serviceIntent)
+        }
+
+        binding.btnNextActivity.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
         }
     }
 }
